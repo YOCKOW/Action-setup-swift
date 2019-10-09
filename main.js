f9117d39020997cbbcb99affe6688bcfe844d1c8
YOCKOW <YOCKOW@users.noreply.github.com>
2019-10-09 14:26:35 +0900

    Select (almost appropriate) SDK for development snapshots.
    Cherry-pick from f0accb7020fb8c3d947555a6abdd8337b7a37ddf (@1.0.x/experiment).

--- main.js
+++ main.js
@@ -9,6 +9,8 @@ var __importStar = (this && this.__importStar) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const core = __importStar(require("@actions/core"));
 const exec = __importStar(require("@actions/exec"));
+const os = __importStar(require("os"));
+const semver = __importStar(require("semver"));
 const swiftVersion = core.getInput('swift-version');
 const workingDirectory = '$HOME/action-setup-swift';
 const swiftenvDirectory = `${workingDirectory}/.swiftenv`;
@@ -32,7 +34,7 @@ async function download_swiftenv() {
 }
 async function check_swift() {
     let installed = false;
-    await run('Checking whether or not Swift ' + swiftVersion + ' is already installed.', async () => {
+    await run('Check whether or not Swift ' + swiftVersion + ' is already installed.', async () => {
         let status = await exec.exec('swiftenv', ['prefix', swiftVersion], {
             ignoreReturnCode: true
         });
@@ -45,6 +47,51 @@ async function download_swift() {
         await exec.exec(swiftenvPath, ['install', swiftVersion]);
     });
 }
+async function semantic_version_of_xcode_for_path(path) {
+    let versionString = '';
+    await exec.exec('defaults', ['read', `${path}/Contents/Info`, 'CFBundleShortVersionString'], {
+        listeners: {
+            stdout: (data) => { versionString = data.toString().trim(); }
+        }
+    });
+    if ((/^\d+\.\d+$/).test(versionString)) {
+        versionString += '.0';
+    }
+    return semver.parse(versionString, true);
+}
+let _xcode_info_list = [];
+async function xcode_info_list() {
+    if (_xcode_info_list.length < 1) {
+        await run('Search Installed Xcode Applications...', async () => {
+            let paths = [];
+            await exec.exec('mdfind', ["kMDItemCFBundleIdentifier == \"com.apple.dt.Xcode\""], {
+                ignoreReturnCode: true,
+                listeners: {
+                    stdout: (data) => {
+                        paths = data.toString().split(/\r\n|\r|\n/).map(path => path.trim()).filter(path => path != '');
+                    }
+                }
+            });
+            for (let ii = 0; ii < paths.length; ii++) {
+                const version = await semantic_version_of_xcode_for_path(paths[ii]);
+                const info = { path: paths[ii], version: version };
+                // core.info(`The version of Xcode at "${info.path}" is ${info.version}.`);
+                _xcode_info_list.push(info);
+            }
+        });
+    }
+    return _xcode_info_list;
+}
+async function latest_xcode_info() {
+    const list = await xcode_info_list();
+    let latest = null;
+    list.forEach((info) => {
+        if ((info.version) && (!latest || semver.gt(info.version, latest.version))) {
+            latest = info;
+        }
+    });
+    return latest;
+}
 async function switch_swift() {
     await run('Switch Swift to ' + swiftVersion, async () => {
         let swiftPath = '';
@@ -55,14 +102,27 @@ async function switch_swift() {
                 stdout: (data) => { swiftPath = data.toString().trim(); }
             }
         });
-        let swiftBinDirectory = swiftPath.replace(/\/swift$/, '');
+        const swiftBinDirectory = swiftPath.replace(/\/swift$/, '');
         core.addPath(swiftBinDirectory);
         // FIXME: There should be more appropriate way...
-        let xcodePathRegExp = new RegExp('^/Applications/Xcode[^/]*.app/Contents/Developer');
-        let result = swiftBinDirectory.match(xcodePathRegExp);
-        if (result && result[0]) {
-            await exec.exec('sudo xcode-select', ['-switch', result[0]]);
+        if (os.platform() != 'darwin') {
+            return;
         }
+        const xcodePathRegExp = new RegExp('^/Applications/Xcode[^/]*.app/Contents/Developer');
+        const xcodeMatched = swiftBinDirectory.match(xcodePathRegExp);
+        let developerDirectory = '/Applications/Xcode.app/Contents/Developer';
+        if (xcodeMatched && xcodeMatched[0]) {
+            developerDirectory = xcodeMatched[0];
+        }
+        else {
+            const latestXcodeInfo = await latest_xcode_info();
+            if (latestXcodeInfo) {
+                developerDirectory = `${latestXcodeInfo.path}/Contents/Developer`;
+            }
+        }
+        await run(`Switch Developer Directory to ${developerDirectory}`, async () => {
+            await exec.exec('sudo xcode-select', ['-switch', developerDirectory]);
+        });
     });
 }
 async function main() {
