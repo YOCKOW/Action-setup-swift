dc5db98be047cec0b547eba169362f383d383d89
YOCKOW <YOCKOW@users.noreply.github.com>
2019-10-30 17:21:18 +0900

    Split sources: create "xcode.ts".

--- __tests__/xcode.test.ts
+++ __tests__/xcode.test.ts
@@ -1,3 +1,13 @@
-describe('template', () => {
-  test('test', () => { expect('test').toBe('test') })
+import * as os from 'os'
+import * as xcode from '../src/xcode'
+
+describe('Xcode Tests', () => {
+  test('List of Applications', async () => {
+    const list = await xcode.installedXcodeApplications()
+    if (os.platform() == 'darwin') {
+      expect(list.length).toBeGreaterThan(0)
+    } else {
+      expect(list.length).toBe(0)
+    }
+  })
 })
