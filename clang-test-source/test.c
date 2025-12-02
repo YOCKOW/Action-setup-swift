20fc48a20522a296cf86bccfcd070a41df01f9a7
YOCKOW <YOCKOW@users.noreply.github.com>
2025-12-02 12:25:24 +0900

    Test clang.
    Checks https://github.com/YOCKOW/Action-setup-swift/issues/99

---
+++ clang-test-source/test.c
@@ -0,0 +1,12 @@
+/* *************************************************************************************************
+ test.c
+   © 2025 YOCKOW.
+     Licensed under MIT License.
+     See "LICENSE.txt" for more information.
+ ************************************************************************************************ */
+
+#include <stdio.h>
+
+int main(int argc, char *argv[]){
+  printf("Hello, %s!\n", "World");
+}
