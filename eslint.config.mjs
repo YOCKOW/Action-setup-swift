b919f603b8a54623e29d2747b5e96bca36054079
YOCKOW <YOCKOW@users.noreply.github.com>
2026-08-18 16:42:35 +0900

    Fix eslint.config.mjs.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,36 +1,45 @@
 // @ts-check
 
 import js from '@eslint/js';
-import { defineConfig } from 'eslint/config';
+import { defineConfig, globalIgnores } from 'eslint/config';
 import tseslint from 'typescript-eslint';
 
-export default defineConfig({
-  files: [
-    'src/**/*.ts'
-  ],
-  extends: [
-    js.configs.recommended,
-    tseslint.configs.strictTypeChecked,
-  ],
-  languageOptions: {
-    parserOptions: {
-      projectService: true,
+export default defineConfig(
+  globalIgnores([
+    "./clang-test-source",
+    "./lib",
+    "./node_modules",
+    "./swift-test-package",
+    "./utils",
+  ]),
+  {
+    files: [
+      'src/**/*.ts'
+    ],
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.strictTypeChecked,
+    ],
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+      },
     },
-  },
-  rules: {
-    "@typescript-eslint/no-unnecessary-condition": [
-      "error",
-      {
-        allowConstantLoopConditions: true
-      }
-    ],
-    "@typescript-eslint/no-namespace": [
-      "error",
-      {
-        allowDeclarations: true,
-        allowDefinitionFiles: false
-      }
-    ],
-    "@typescript-eslint/strict-boolean-expressions": "error",
+    rules: {
+      "@typescript-eslint/no-unnecessary-condition": [
+        "error",
+        {
+          allowConstantLoopConditions: true
+        }
+      ],
+      "@typescript-eslint/no-namespace": [
+        "error",
+        {
+          allowDeclarations: true,
+          allowDefinitionFiles: false
+        }
+      ],
+      "@typescript-eslint/strict-boolean-expressions": "error",
+    }
   }
-});
+);
